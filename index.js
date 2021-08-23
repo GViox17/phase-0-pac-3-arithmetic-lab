bcf37e6ca872e9fe66f2329961c8616ff61d3cd6
Gabriel Viox <gviox17@gmail.com>
2021-08-23 12:32:28 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(first,second) {
+    return first + second
+}
+function subtract(first,second) {
+    return first - second
+}
+function multiply(first,second) {
+    return first * second
+}
+function divide(first,second) {
+    return first / second
+}
+function increment(up){
+    return up+1
+}
+function decrement(down){
+    return down-1
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
